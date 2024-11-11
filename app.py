0dbb5c4f34419f72315f85795b56669be4159839
Manuel Sanchez <szmanuel23@gmail.com>
2024-11-11 14:34:13 -0600

    Webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,12 +21,6 @@ class Log(db.Model):
 with app.app_context():
     db.create_all()
 
-    prueba1 = Log(texto='Mensaje de Prueba 1')
-    prueba2 = Log(texto='Mensaje de Prueba 2')
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
 
 #Funcion para ordenar los registros por fecha y hora 
 def ordenar_por_fecha_y_hora(registros):
@@ -50,6 +44,36 @@ def agregar_mensajes_log(texto):
     nuevo_registro= Log(texto=texto)
     db.session.add(nuevo_registro)
     db.session.commit()
+
+#Token de verificación para la configurción
+
+TOKEN_MANUEL= "Manuel23"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method== 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method== 'POST':
+        response = recibir_mensajes(request)
+        return response
+
+def verificar_token(req):
+    token=req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_MANUEL:
+        return challenge
+    else:
+        return jsonify({'error': 'Token invalido'}),401
+
+
+def recibir_mensajes(req):
+    req = request.get_json()
+    agregar_mensajes_log(req)
+    
+    return jsonify({'message': 'EVENT_RECEIVED'})
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
